dd89df2e507676c4f71882ca1f0c9fd04d11aa1b
Guillaume Maille <48036692+Guillaume-Maille@users.noreply.github.com>
2020-05-20 09:42:18 +0800

    Update Task 4.py

--- Booksort/Task 4.py
+++ Booksort/Task 4.py
@@ -1,11 +1,3 @@
-# TASK 4 – Algorithm modification
-# Additional information needs to be saved for each book, such as a publication date.
-# An additional task is needed to create a new file from the original file. The task will prompt the user to
-# input the additional information for each book.
-# Consider possible validation of the additional information.
-# Write program code for the additional task c.
-
-
 def add_info():
     column = input("Add new info:")
     with open('Books.txt') as c:
